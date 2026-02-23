830997d1592aecec8931336ba0a63172a726c434
aalvarez6 <alejandra.alvareza67@gmail.com>
2026-02-22 20:27:54 -0500

    Update Intro.py

--- Intro.py
+++ Intro.py
@@ -6,5 +6,11 @@ st.title("Mi primera APP interactiva")
 st.write ("Determinar que tan bien estoy haciendo los estiramientos de YOGA")
 #Entrada de texto
 nombre = st.text_input ("Escribe tu nombre:")
+# Boton
+if st.button ("Saludar"):
+  st.success(f"hola {nombre}, bienvenido a YoGiOH!)
 
+# Slider
+edad = st.slider("seleciona tu edad", 0, 100, 25)
+st.write("Tu edad es:", edad)
 
